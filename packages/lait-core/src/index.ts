30d91c4e4488a955cfbaaaa5f3f1ab748abe4d58
Alex Bowen <ajbowen249@gmail.com>
2022-12-08 21:44:16 -0500

    refactor args

--- packages/lait-core/src/index.ts
+++ packages/lait-core/src/index.ts
@@ -3,95 +3,47 @@ import * as fs from 'fs/promises';
 import * as tsNode from 'ts-node';
 import { transpile } from './transpiler';
 import { exit } from 'process';
+import { getArgs, ArgSchema, describe } from './args';
 
-interface ParsedArgs {
-    named: { [index: string]: string|number|boolean|undefined };
-    positional: string[];
-}
+const argsSchema: ArgSchema = {
+    aliases: { t: 'transpileOnly', f: 'file', h: 'help' },
+    named: {
+        transpileOnly: {
+            description: 'Only print transpiled file and exit. Do not execute.',
+            isFlag: true,
+            parseAs: 'boolean',
+        },
+        file: {
+            description: 'Open script file in lieu of passing a positional arg',
+        },
+        help: {
+            description: 'Print help text and exit.',
+            isFlag: true,
+            parseAs: 'boolean',
+        },
+    },
+    positional: [
+        { name: 'script' },
+        { name: 'input' },
+    ],
+};
 
-interface ArgSchema {
-    aliases: { [index: string]: string },
-    named: { [index: string]: { isFlag?: boolean, parseAs?: 'number' | 'boolean' } },
-}
-
-function getArgs(schema: ArgSchema) {
-    const args: ParsedArgs = { named: {}, positional: [] };
-    // First pull out non-positional args
-    const rawArgs: (string|undefined)[] = process.argv.slice(2);
-    for (let i = 0; i < rawArgs.length; i++) {
-        const arg = rawArgs[i];
-        if (!arg) {
-            continue;
-        }
-
-        if (arg.startsWith('-')) {
-            const strippedDash = arg.replace(/^-{1,2}/, '');
-            const getFullName = (name: string) => name in schema.aliases ? schema.aliases[name] : name;
-            const getValue = (name: string, value: string) => {
-                const parseAs = schema.named[name].parseAs;
-                if (!parseAs) {
-                    return value;
-                }
-
-                if (parseAs === 'number') {
-                    return parseFloat(value);
-                } else {
-                    return value.toLowerCase() === 'true';
-                }
-            };
-
-            if (strippedDash.includes('=')) {
-                const parts = strippedDash.split('=');
-                const argName = getFullName(parts[0]);
-                args.named[argName] = getValue(argName, parts[1]);
-                rawArgs[i] = undefined;
-            } else {
-                const argName = getFullName(strippedDash);
-                if (schema.named[argName]?.isFlag) {
-                    args.named[argName] = true;
-                    rawArgs[i] = undefined;
-                } else {
-                    if (i === rawArgs.length - 1 ) {
-                        console.error(`Missing value for flag ${argName}`);
-                        exit(-1);
-                    } else {
-                        const value = rawArgs[i + 1]!;
-                        args.named[argName] = getValue(argName, value);
-                        rawArgs[i] = undefined;
-                        rawArgs[i + 1] = undefined;
-                    }
-                }
-            }
-        }
-    }
-
-    // now do positional
-    for (let i = 0; i < rawArgs.length; i++) {
-        const arg = rawArgs[i];
-        if (arg) {
-            args.positional.push(arg);
-        }
-    }
-
-    return args;
-}
-
-interface Args {
+interface AppArgs {
     named: {
         transpileOnly?: boolean;
         file?: string;
+        help?: boolean;
     },
     positional: string[],
 }
 
 async function main() {
-    const args = getArgs({
-        aliases: { t: 'transpileOnly', f: 'file' },
-        named: {
-            transpileOnly: { isFlag: true, parseAs: 'boolean' },
-            file: { },
-        },
-    }) as Args;
+    const args = getArgs(argsSchema) as AppArgs;
+
+    if (args.named.help) {
+        describe(argsSchema);
+        exit(0);
+    }
 
     let inputScript: string | undefined;
     let inputFileNameIndex = 1;
