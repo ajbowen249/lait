e254559f51668514cc8d0f25114b0773ef854a27
Alex Bowen <ajbowen249@gmail.com>
2022-12-08 20:34:46 -0500

    more fixes

--- packages/lait/src/index.ts
+++ packages/lait/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as fs from 'fs/promises';
 import * as tsNode from 'ts-node';
-import { transpile } from 'transpiler/dist';
+import { transpile } from 'transpiler';
 import { exit } from 'process';
 
 interface ParsedArgs {
