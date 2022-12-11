27dbf4e457fc7248c0b6ec41ec2604935ac548c4
Alex Bowen <ajbowen249@gmail.com>
2022-12-10 23:57:22 -0500

    fully runnable playground

--- packages/lait-docs-site/src/components/LaitPlayground.vue
+++ packages/lait-docs-site/src/components/LaitPlayground.vue
@@ -1,9 +1,9 @@
 <template>
-    <label for="scriptInput">Script</label><br />
-    <textarea v-model="scriptInput" id="scriptInput" cols="80" /><br />
-
     <label for="fileInput">Input</label><br />
     <textarea v-model="fileInput" id="fileInput" cols="80" rows="10" /><br />
+
+    <label for="scriptInput">Script</label><br />
+    <textarea v-model="scriptInput" id="scriptInput" cols="80" /><br />
 
     <button @click="run">Run</button><br />
 
@@ -16,16 +16,83 @@ import { ref } from 'vue';
 import * as t from '../../../lait-core/src/transpiler';
 import * as ts from 'typescript';
 
-const scriptInput = ref('');
-const fileInput = ref('');
+const scriptInput = ref('/#\\w{5} \\w+ \\d+/; { print($[2], $[1]) }');
+const fileInput = ref(`OrderId Item Quantity
+#a2fr5 Socks 16
+#d8j38 Avocado 2
+#8fh39 Shampoo 1
+#qb6ag Candle 3`);
 const consoleOutput = ref('');
 
-function print(...args: string[]) {
-    (window as any).appendOutputText(args.join(' '));
+const browserTemplate = `
+let FS = ' ';
+let TRIM_EMPTY = true;
+
+// for convenience
+const print = console.log.bind(console);
+
+const LAIT_PROGRAM_INPUT_LINES = [
+    // BROWSER_INPUT_LINES
+];
+
+type HandlerFunc = ($: string[], m: RegExpMatchArray, g?: RegExpMatchArray['groups']) => Promise<void>;
+
+interface Handler {
+    regex: RegExp;
+    handler: HandlerFunc,
+}
+
+async function main() {
+    // INIT_STATEMENTS
+
+    const LAIT_PROGRAM_HANDLERS: Handler[] = [
+        // HANDLERS
+    ];
+
+    let LAIT_DEFAULT_HANDLER = async ($: string[]) => {
+        // DEFAULT_HANDLER
+    };
+
+    const processLine = async (line: string) => {
+        let handled = false;
+        let fields = line.split(FS);
+        if (TRIM_EMPTY) {
+            fields = fields.filter(x => x !== '');
+        }
+
+        for (const handler of LAIT_PROGRAM_HANDLERS) {
+            const match = line.match(handler.regex);
+            if (match) {
+                await handler.handler(fields, match, match.groups);
+                handled = true;
+                break;
+            }
+        }
+
+        if (!handled) {
+            await LAIT_DEFAULT_HANDLER(fields);
+        }
+    };
+
+    for (const line of LAIT_PROGRAM_INPUT_LINES) {
+        processLine(line);
+    }
+
+    // END_STATEMENTS
+}
+
+main();
+`;
+
+function getScriptTemplate() {
+    return browserTemplate.replace(
+        '// BROWSER_INPUT_LINES',
+        fileInput.value.split('\n').map(x => `'${x.replace(/\\/, '\\').replace(/'/, '\\\'')}'`).join(',\n'),
+    );
 }
 
 function run() {
-    t.transpile(scriptInput.value, '', '// INIT_STATEMENTS').then(x => {
+    t.transpile(scriptInput.value, '', getScriptTemplate()).then(x => {
         const js = ts.transpile(x);
         const consoleLog = console.log;
         console.log = (...data: any[]) => {
