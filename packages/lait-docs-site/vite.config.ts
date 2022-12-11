2eebb7b17122ffd3f2da13b4a73df21a5ef19cfb
Alex Bowen <ajbowen249@gmail.com>
2022-12-11 14:03:31 -0500

    fixup

--- packages/lait-docs-site/vite.config.ts
+++ packages/lait-docs-site/vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: 'https://ajbowen249.github.io/lait',
+  base: 'lait',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
