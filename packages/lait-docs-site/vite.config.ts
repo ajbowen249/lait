1bea31eeed85f63e2c0aafe01333adc283d5912a
Alex Bowen <ajbowen249@gmail.com>
2022-12-11 13:58:29 -0500

    add built site

--- packages/lait-docs-site/vite.config.ts
+++ packages/lait-docs-site/vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: 'https://ajbowen249.github.io/lait',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
